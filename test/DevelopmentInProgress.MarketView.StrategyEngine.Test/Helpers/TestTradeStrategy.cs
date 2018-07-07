e3567bffdaf20bad0fe2598f9497c1cca877380f
grantcolley <grantcolley@yahoo.com>
2018-07-07 13:09:51 +0100

    Add TestTradeStrategy and test data.
    Add TestTradeStrategy and test data.

---
+++ test/DevelopmentInProgress.MarketView.StrategyEngine.Test/Helpers/TestTradeStrategy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DevelopmentInProgress.MarketView.Interface.Events;
+using DevelopmentInProgress.MarketView.Interface.Model;
+using DevelopmentInProgress.MarketView.Interface.TradeStrategy;
+
+namespace DevelopmentInProgress.MarketView.StrategyEngine.Test.Helpers
+{
+    public class TestTradeStrategy : ITradeStrategy
+    {
+        public event EventHandler<TradeStrategyNotificationEventArgs> TradeStrategyNotificationEvent;
+
+        public IEnumerable<AggregateTrade> AggregateTrades { get; set; }
+
+        public Task<Strategy> RunAsync(Strategy strategy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeAccountInfo(AccountInfoEventArgs accountInfoEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeAccountInfoException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeAggregateTrades(AggregateTradeEventArgs aggregateTradeEventArgs)
+        {
+            AggregateTrades = aggregateTradeEventArgs.AggregateTrades;
+        }
+
+        public void SubscribeAggregateTradesException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeOrderBook(OrderBookEventArgs orderBookEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeOrderBookException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeStatistics(StatisticsEventArgs statisticsEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeStatisticsException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
