ac9d793482e714655f5b94c958710e5149275c57
grantcolley <grantcolley@yahoo.com>
2018-07-15 20:47:00 +0100

    test in progress...
    test in progress...

--- test/DevelopmentInProgress.MarketView.StrategyEngine.Test/SubscriptionsCacheManagerTests.cs
+++ test/DevelopmentInProgress.MarketView.StrategyEngine.Test/SubscriptionsCacheManagerTests.cs
@@ -121,5 +121,134 @@ namespace DevelopmentInProgress.MarketView.StrategyEngine.Test
                 Assert.IsFalse(test.HasSubscriptions);
             }
         }
+
+        [TestMethod]
+        public void Subscribe_MultipleSymbols_SingleSubscription_Subscribe()
+        {
+            // Arrange
+            var exchangeServiceFactory = new TestExchangeServiceFactory();
+            var subscriptionsCacheFactory = new TestSubscriptionsCacheFactory(exchangeServiceFactory);
+
+            var strategy1 = new Strategy { Name = "Test 1" };
+
+            var trxBinance = new StrategySubscription
+            {
+                Exchange = Exchange.Binance,
+                Symbol = "TRXBTC-BINANCE",
+                Subscribe = (Subscribe.AggregateTrades)
+            };
+
+            strategy1.StrategySubscriptions.Add(trxBinance);
+
+            var strategy2 = new Strategy { Name = "Test 2" };
+
+            var trxTest = new StrategySubscription
+            {
+                Exchange = Exchange.Test,
+                Symbol = "TRXBTC-TEST",
+                Subscribe = (Subscribe.AggregateTrades)
+            };
+
+            strategy2.StrategySubscriptions.Add(trxTest);
+
+            var tradeStrategy1 = new TestTradeStrategy();
+
+            var tradeStrategy2 = new TestTradeStrategy();
+
+            using (var subscriptionsCacheManager = new SubscriptionsCacheManager(subscriptionsCacheFactory))
+            {
+                // Act
+                subscriptionsCacheManager.Subscribe(strategy1, tradeStrategy1);
+
+                subscriptionsCacheManager.Subscribe(strategy2, tradeStrategy2);
+
+                // Assert
+                var binance = subscriptionsCacheManager.SubscriptionsCacheFactory.GetSubscriptionsCache(Exchange.Binance);
+
+                Assert.IsTrue(binance.HasSubscriptions);
+
+                if (binance.Caches.TryGetValue("TRXBTC-BINANCE", out ISubscriptionCache trxBinanceCache))
+                {
+                    Assert.IsNotNull(trxBinanceCache);
+                    Assert.IsInstanceOfType(trxBinanceCache, typeof(BinanceSymbolSubscriptionCache));
+                    Assert.IsTrue(trxBinanceCache.HasSubscriptions);
+                    Assert.AreEqual(trxBinanceCache.Subscriptions(Subscribe.AggregateTrades), 1);
+                }
+                else
+                {
+                    Assert.Fail();
+                }
+
+                var test = subscriptionsCacheManager.SubscriptionsCacheFactory.GetSubscriptionsCache(Exchange.Test);
+
+                Assert.IsTrue(test.HasSubscriptions);
+
+                if (test.Caches.TryGetValue("TRXBTC-TEST", out ISubscriptionCache trxTestCache))
+                {
+                    Assert.IsNotNull(trxTestCache);
+                    Assert.IsInstanceOfType(trxTestCache, typeof(TestSubscriptionCache));
+                }
+                else
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+
+        [TestMethod]
+        public async Task Subscribe_MultipleSymbols_SingleSubscription_Unsubscribe()
+        {
+            // Arrange
+            var exchangeServiceFactory = new TestExchangeServiceFactory();
+            var subscriptionsCacheFactory = new TestSubscriptionsCacheFactory(exchangeServiceFactory);
+
+            var strategy1 = new Strategy { Name = "Test 1" };
+
+            var trxBinance = new StrategySubscription
+            {
+                Exchange = Exchange.Binance,
+                Symbol = "TRXBTC-BINANCE",
+                Subscribe = (Subscribe.AggregateTrades)
+            };
+
+            strategy1.StrategySubscriptions.Add(trxBinance);
+
+            var strategy2 = new Strategy { Name = "Test 2" };
+
+            var trxTest = new StrategySubscription
+            {
+                Exchange = Exchange.Test,
+                Symbol = "TRXBTC-TEST",
+                Subscribe = (Subscribe.AggregateTrades)
+            };
+
+            strategy2.StrategySubscriptions.Add(trxTest);
+
+            var tradeStrategy1 = new TestTradeStrategy();
+            var tradeStrategy2 = new TestTradeStrategy();
+
+            using (var subscriptionsCacheManager = new SubscriptionsCacheManager(subscriptionsCacheFactory))
+            {
+                // Act
+                subscriptionsCacheManager.Subscribe(strategy1, tradeStrategy1);
+
+                subscriptionsCacheManager.Subscribe(strategy2, tradeStrategy2);
+
+                await Task.Delay(1000);
+
+                subscriptionsCacheManager.Unsubscribe(strategy1, tradeStrategy1);
+
+                subscriptionsCacheManager.Unsubscribe(strategy2, tradeStrategy2);
+
+                // Assert
+                var binance = subscriptionsCacheManager.SubscriptionsCacheFactory.GetSubscriptionsCache(Exchange.Binance);
+
+                Assert.IsFalse(binance.HasSubscriptions);
+
+                var test = subscriptionsCacheManager.SubscriptionsCacheFactory.GetSubscriptionsCache(Exchange.Test);
+
+                Assert.IsFalse(test.HasSubscriptions);
+            }
+        }
     }
 }
