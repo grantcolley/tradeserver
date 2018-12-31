b9919e10c52baf94b82ce3e4b17be8c12d4a1e5a
grantcolley <grantcolley@yahoo.com>
2018-12-31 19:20:06 +0000

    Set MessageType = MessageType.SendToChannel
    Set MessageType = MessageType.SendToChannel

--- src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/Notification/Publishing/NotificationPublisherContext.cs
+++ src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/Notification/Publishing/NotificationPublisherContext.cs
@@ -18,28 +18,28 @@ namespace DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Notification.
         public async Task PublishNotificationsAsync(string strategyName, IEnumerable<StrategyNotification> notification)
         {
             var json = JsonConvert.SerializeObject(notification);
-            var msg = new Message { SenderConnectionId = strategyName, MethodName = "Notification", Data = json };
+            var msg = new Message { SenderConnectionId = strategyName, MessageType = MessageType.SendToChannel, MethodName = "Notification", Data = json };
             await notificationHub.SendMessageToChannelAsync(strategyName, msg);
         }
 
         public async Task PublishTradesAsync(string strategyName, IEnumerable<StrategyNotification> notification)
         {
             var json = JsonConvert.SerializeObject(notification);
-            var msg = new Message { SenderConnectionId = strategyName, MethodName = "Trade", Data = json };
+            var msg = new Message { SenderConnectionId = strategyName, MessageType = MessageType.SendToChannel, MethodName = "Trade", Data = json };
             await notificationHub.SendMessageToChannelAsync(strategyName, msg);
         }
 
         public async Task PublishOrderBookAsync(string strategyName, IEnumerable<StrategyNotification> notification)
         {
             var json = JsonConvert.SerializeObject(notification);
-            var msg = new Message { SenderConnectionId = strategyName, MethodName = "OrderBook", Data = json };
+            var msg = new Message { SenderConnectionId = strategyName, MessageType = MessageType.SendToChannel, MethodName = "OrderBook", Data = json };
             await notificationHub.SendMessageToChannelAsync(strategyName, msg);
         }
 
         public async Task PublishAccountInfoAsync(string strategyName, IEnumerable<StrategyNotification> notification)
         {
             var json = JsonConvert.SerializeObject(notification);
-            var msg = new Message { SenderConnectionId = strategyName, MethodName = "AccountInfo", Data = json };
+            var msg = new Message { SenderConnectionId = strategyName, MessageType = MessageType.SendToChannel, MethodName = "AccountInfo", Data = json };
             await notificationHub.SendMessageToChannelAsync(strategyName, msg);
         }
     }
