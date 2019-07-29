243ef8ef7f94a4a3bd49ee8a748068075abe53e8
grantcolley <grantcolley@yahoo.com>
2019-07-29 20:31:49 +0100

    Update NotificationHub.cs

--- src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/Notification/Publishing/NotificationHub.cs
+++ src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/Notification/Publishing/NotificationHub.cs
@@ -40,19 +40,7 @@ namespace DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Notification.
 
             await SendMessageAsync(websocket, message).ConfigureAwait(false);
         }
-
-        //public override async Task OnConnectedAsync()
-        //{
-        //    var strategyName = Context.GetHttpContext().Request.Query["strategyname"];
-        //    await Groups.AddToGroupAsync(Context.ConnectionId, strategyName);
-        //    await Clients.Client(Context.ConnectionId).SendAsync("Connected", $"Connected and listening for notifications from Strategy {strategyName}. ConnectionId {Context.ConnectionId}");
-        //}
-
-        //public override Task OnDisconnectedAsync(Exception exception)
-        //{
-        //    return base.OnDisconnectedAsync(exception);
-        //}
-
+        
         public async override Task ReceiveAsync(WebSocket webSocket, Message message)
         {
             try
