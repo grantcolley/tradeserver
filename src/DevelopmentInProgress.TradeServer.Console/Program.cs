b5e23586e2515627270acb3432695701209ba844
grantcolley <grantcolley@yahoo.com>
2020-02-13 19:11:59 +0000

    Update Program.cs

--- src/DevelopmentInProgress.TradeServer.Console/Program.cs
+++ src/DevelopmentInProgress.TradeServer.Console/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Serilog;
 using System;
+using System.Linq;
 
 namespace DevelopmentInProgress.TradeServer.Console
 {
@@ -10,18 +11,6 @@ namespace DevelopmentInProgress.TradeServer.Console
     {
         static void Main(string[] args)
         {
-            if (args == null
-                || args.Length.Equals(0))
-            {
-                args = new[] 
-                {
-                    $"ServerName=TradeServer_{Guid.NewGuid().ToString()}", 
-                    "Url=http://+:5500" 
-                };
-            }
-
-            var url = args[1].Split("=")[1];
-
             try
             {
                 Log.Logger = new LoggerConfiguration()
@@ -29,12 +18,41 @@ namespace DevelopmentInProgress.TradeServer.Console
                     .WriteTo.Console()
                     .CreateLogger();
 
-                Log.Information("Launching DevelopmentInProgress.TradeServer.Console");
+                if (args == null
+                    || args.Length.Equals(0))
+                {
+                    Log.Warning($"No args so use defaults...");
+
+                    args = new[]
+                    {
+                        $"ServerName=TradeServer_{Guid.NewGuid().ToString()}",
+                        "Url=http://+:5500"
+                    };
+                }
+                else if (InValidArgs(args))
+                {
+                    Log.Error($"Invalid args");
+
+                    foreach (var arg in args)
+                    {
+                        Log.Error($"{arg}");
+                    }
+
+                    Log.Error($"You must provide the following args:");
+                    Log.Error($"--ServerName=YourServerName");
+                    Log.Error($"--Url=http://+:5500");
+
+                    return;
+                }
 
                 foreach (var arg in args)
                 {
                     Log.Information($"{arg}");
                 }
+
+                var url = args.First(a => a.StartsWith("Url=")).Split("=")[1];
+
+                Log.Information("Launching DevelopmentInProgress.TradeServer.Console");
 
                 var webHost = WebHost.CreateDefaultBuilder()
                     .UseUrls(url)
@@ -53,6 +71,29 @@ namespace DevelopmentInProgress.TradeServer.Console
             {
                 Log.CloseAndFlush();
             }
+        }
+
+        private static bool InValidArgs(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                return true;
+            }
+            
+            if(!args[0].StartsWith("--ServerName=") && !args[1].StartsWith("--ServerName="))
+            {
+                return true;
+            }
+
+            if(!args[0].StartsWith("--Url=") && !args[1].StartsWith("--Url="))
+            {
+                return true;
+            }
+
+            args[0] = args[0].Substring(2, args[0].Length - 2);
+            args[1] = args[1].Substring(2, args[1].Length - 2);
+
+            return false;
         }
     }
 }
