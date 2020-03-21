b6e1da23d3d75476b73bc42206547ee41bf53bac
grantcolley <grantcolley@yahoo.com>
2020-03-21 21:22:09 +0000

    Update ServerBatchNotificationPublisher.cs

--- src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/Notification/Server/ServerBatchNotificationPublisher.cs
+++ src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/Notification/Server/ServerBatchNotificationPublisher.cs
@@ -1,7 +1,5 @@
 ﻿using DevelopmentInProgress.TradeView.Interface.Server;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
