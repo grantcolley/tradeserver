881dfdb55117ef55e634e06645a768ec4d49199f
grantcolley <grantcolley@yahoo.com>
2020-03-21 11:39:51 +0000

    Implement IServerNotification and ServerNotificationBase
    Implement IServerNotification and ServerNotificationBase

--- src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/Cache/TradeStrategy/ITradeStrategyCacheManager.cs
+++ src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/Cache/TradeStrategy/ITradeStrategyCacheManager.cs
@@ -1,15 +1,17 @@
-﻿using DevelopmentInProgress.TradeView.Interface.Strategy;
+﻿using DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Notification.Server;
+using DevelopmentInProgress.TradeView.Interface.Strategy;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Cache.TradeStrategy
 {
-    public interface ITradeStrategyCacheManager
+    public interface ITradeStrategyCacheManager : IServerNotification
     {
         bool TryAddTradeStrategy(string strategyName, ITradeStrategy tradeStrategy);
         bool TryRemoveTradeStrategy(string strategyName, out ITradeStrategy tradeStrategy);
         bool TryGetTradeStrategy(string strategyName, out ITradeStrategy tradeStrategy);
         Task StopStrategy(string strategyName, string parameters);
         Task UpdateStrategy(string strategyName, string parameters);
-        void ServerNotification();
+        List<Strategy> GetStrategies();
     }
 }
--- src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/Cache/TradeStrategy/TradeStrategyCacheManager.cs
+++ src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/Cache/TradeStrategy/TradeStrategyCacheManager.cs
@@ -1,32 +1,24 @@
 ﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
-using DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Notification;
-using DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Notification.Strategy;
-using DevelopmentInProgress.TradeView.Interface.Server;
+using DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Notification.Server;
 using DevelopmentInProgress.TradeView.Interface.Strategy;
 
 namespace DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Cache.TradeStrategy
 {
-    public class TradeStrategyCacheManager : ITradeStrategyCacheManager
+    public class TradeStrategyCacheManager : ServerNotificationBase, ITradeStrategyCacheManager
     {
-        private IServerMonitor serverMonitor;
-        private IBatchNotification<ServerNotification> serverBatchNotificationPublisher;
-        private StrategyNotificationHub strategyNotificationHub;
         private ConcurrentDictionary<string, ITradeStrategy> tradeStrategies;
-        private SemaphoreSlim notificationSemaphoreSlim = new SemaphoreSlim(1, 1);
 
-        public TradeStrategyCacheManager(
-            IServerMonitor serverMonitor, 
-            IBatchNotification<ServerNotification> serverBatchNotificationPublisher,
-            StrategyNotificationHub strategyNotificationHub)
+        public TradeStrategyCacheManager()
         {
-            this.serverMonitor = serverMonitor;
-            this.strategyNotificationHub = strategyNotificationHub;
-            this.serverBatchNotificationPublisher = serverBatchNotificationPublisher;
             tradeStrategies = new ConcurrentDictionary<string, ITradeStrategy>();
+        }
+
+        public List<Strategy> GetStrategies()
+        {
+            return tradeStrategies.Values.Select(s => s.Strategy).ToList();
         }
 
         public bool TryGetTradeStrategy(string strategyName, out ITradeStrategy tradeStrategy)
@@ -38,7 +30,7 @@ namespace DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Cache.TradeSt
         {
             if(tradeStrategies.TryAdd(strategyName, tradeStrategy))
             {
-                ServerNotification();
+                OnServerNotification();
                 return true;
             }
 
@@ -49,7 +41,7 @@ namespace DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Cache.TradeSt
         {
             if (tradeStrategies.TryRemove(strategyName, out tradeStrategy))
             {
-                ServerNotification();
+                OnServerNotification();
                 return true;
             }
 
@@ -62,7 +54,7 @@ namespace DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Cache.TradeSt
             {
                 await tradeStrategy.TryStopStrategy(parameters);
 
-                ServerNotification();
+                OnServerNotification();
             }
         }
 
@@ -72,40 +64,7 @@ namespace DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Cache.TradeSt
             {
                 await tradeStrategy.TryUpdateStrategyAsync(parameters);
 
-                ServerNotification();
-            }
-        }
-
-        public void ServerNotification()
-        {
-            notificationSemaphoreSlim.Wait();
-
-            try
-            {
-                List<ServerStrategy> strategyServers = new List<ServerStrategy>();
-
-                var strategies = tradeStrategies.Values.Select(s => s.Strategy).ToList();
-                var serverInfo = strategyNotificationHub.GetServerInfo();
-
-                var serverStrategies = (from s in strategies
-                                                 join c in serverInfo.Channels on s.Name equals c.Name
-                                                 select new ServerStrategy
-                                                 {
-                                                     Strategy = s,
-                                                     Connections = new List<ServerStrategyConnection>(
-                                                         c.Connections.Select(conn => new ServerStrategyConnection 
-                                                         {
-                                                             Connection = conn.Name 
-                                                         }))
-                                                 }).ToList();
-
-                var serverNotification = serverMonitor.GetServerNotification(serverStrategies);
-
-                serverBatchNotificationPublisher.AddNotification(serverNotification);
-            }
-            finally
-            {
-                notificationSemaphoreSlim.Release();
+                OnServerNotification();
             }
         }
     }
