be2ffaa385e7f7671e3ec14f77f93222d26d66c7
grantcolley <grantcolley@yahoo.com>
2020-03-21 11:39:29 +0000

    Add IServerNotification and ServerNotificationBase

---
+++ src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/Notification/Server/IServerNotification.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.Notification.Server
+{
+    public interface IServerNotification
+    {
+        event EventHandler<ServerNotificationEventArgs> ServerNotification;
+    }
+}
