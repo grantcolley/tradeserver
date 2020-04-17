58cf4ef969b89b8d2bc2eb778b8fe337a9292e41
grantcolley <grantcolley@yahoo.com>
2020-04-17 09:08:48 +0100

    Remove obsolete ExchangeService
    Remove obsolete ExchangeService

--- src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/ExchangeService/ExchangeServiceFactory.cs
+++
@@ -1,7 +0,0 @@
-﻿//namespace DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.ExchangeService
-//{
-//    public abstract class ExchangeServiceFactory<T> : IExchangeServiceFactory<T>
-//    {
-//        public abstract T GetExchangeService(MarketView.Interface.Strategy.Exchange exchange);
-//    }
-//}
--- src/DevelopmentInProgress.TradeServer.StrategyRunner.WebHost/ExchangeService/IExchangeServiceFactory.cs
+++
@@ -1,7 +0,0 @@
-﻿//namespace DevelopmentInProgress.TradeServer.StrategyRunner.WebHost.ExchangeService
-//{
-//    public interface IExchangeServiceFactory<T>
-//    {
-//        T GetExchangeService(MarketView.Interface.Strategy.Exchange exchange);
-//    }
-//}
